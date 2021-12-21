61ef640bcf16814f5c25704e2ee500e4661ad12b
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-12-20 20:54:56 -0800

    add changes to allow for ui

--- src/main.js
+++ src/main.js
@@ -3,3 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
 
+$(document).ready(function(event){
+  event.preventDefault();
+  $("#submit").submit(function(){
+    const inputtedCharName = $("input#characterName").val(); // getting character name from a user
+    const role = $("#roles").val(); // getting selected role from a user
+    let newChar = storeState(role); // new character with role initial stats
+    //let charName = newChar.inputtedCharName;
+    newChar = changeState("name")(inputtedCharName);
+    $("#stats").append(`${newChar}`) // show role stats
+  })
+});
--- src/rpg.js
+++ src/rpg.js
@@ -4,7 +4,7 @@
 //scientist
 //assassin
 
-const storeState = (initialState) => {
+export const storeState = (initialState) => {
   let currentState =initialState;
   return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
@@ -13,60 +13,63 @@ const storeState = (initialState) => {
   }
 }
 
-const warriorInitialValues = {strength: 50, health: 100, resistance: 100, speed: 20, intelligence: 20, agility: 20, charisma: 40}
-const archerInitialValues = {strength: 20, health: 100, resistance: 80, speed: 40, intelligence: 30, agility: 60, charisma: 0}
-const commanderInitialValues = {strength: 30, health: 100, resistance: 100, speed: 20, intelligence: 20, agility: 20, charisma: 100} //
-const scientistInitialValues = {strength: 10, health: 80, resistance: 80, speed: 10, intelligence: 80, agility: 10, charisma: 0}
-const assassinInitialValues = {strength: 50, health: 100, resistance: 80, speed: 50, intelligence: 30, agility: 80, charisma: 0}
+export const warriorInitialValues = {name:"", strength: 50, health: 100, resistance: 100, speed: 20, intelligence: 20, agility: 20, charisma: 40}
+export const archerInitialValues = {name:"", strength: 20, health: 100, resistance: 80, speed: 40, intelligence: 30, agility: 60, charisma: 0}
+export const commanderInitialValues = {name:"", strength: 30, health: 100, resistance: 100, speed: 20, intelligence: 20, agility: 20, charisma: 100} //
+export const scientistInitialValues = {name:"", strength: 10, health: 80, resistance: 80, speed: 10, intelligence: 80, agility: 10, charisma: 0}
+export const assassinInitialValues = {name:"", strength: 50, health: 100, resistance: 80, speed: 50, intelligence: 30, agility: 80, charisma: 0}
 
-const enemyInitialValues = {strength: 50, health: 100}
+export const enemyInitialValues = {strength: 50, health: 100}
 
-const warrior = storeState(warriorInitialValues);
-const archer = storeState(archerInitialValues);
-const commander = storeState(commanderInitialValues);
-const scientist = storeState(scientistInitialValues);
-const assassin = storeState(assassinInitialValues);
+export const warrior = storeState(warriorInitialValues);
+export const archer = storeState(archerInitialValues);
+export const commander = storeState(commanderInitialValues);
+export const scientist = storeState(scientistInitialValues);
+export const assassin = storeState(assassinInitialValues);
 
-const enemy = storeState(enemyInitialValues);
+export const enemy = storeState(enemyInitialValues);
 
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
       ...state,
-      [prop]: (state.name[prop] || 0) + value,
+      [prop]: (state[prop] || 0) + value,
     });
   };
 };
 
-const newCharactor = (name) => {
+export const strengthLevel = changeState("strength")(50);
+export const healthLevel = changeState("health")(50);
+export const resistanceLevel = changeState("resistance")(20);
+export const speedLevel = changeState("speed")(30);
+export const intelligenceLevel = changeState("intelligence")(60);
+export const agilityLevel = changeState("agility")(40);
+export const charismaLevel = changeState("charisma")(0);
+//const comCharismaLevel = changeState("charisma")(50);
+
+export const newCharactor = (name) => {
   let state = {
     name
   }
 
-  return { ...state, ...changeState(state)};
+  return { ...state, ...strengthLevel(newCharactor), ...healthLevel(newCharactor), ...resistanceLevel(newCharactor), ...speedLevel(newCharactor), ...intelligenceLevel(newCharactor), ...agilityLevel(newCharactor), ...charismaLevel(newCharactor)};
 };
 
-const strengthLevel = changeState("strength")(50);
-const healthLevel = changeState("health")(50);
-const resistanceLevel = changeState("resistance")(20);
-const speedLevel = changeState("speed")(30);
-const intelligenceLevel = changeState("intelligence")(60);
-const agilityLevel = changeState("agility")(40);
-const charismaLevel = changeState("chrisma")(0);
 
 // const hit = changeState("health")(`-${state.strength}`)
 
 //state, enemy, property
-// const battle = (state) => {
-//   return (enemy) => {
-//     return (prop) => {
-//       if (state[prop]>enemy[prop]) {
-//         return [state, enemy];
-//       }else if (state[prop]<enemy[prop]) {
-//         return [enemy, state];
-//       }
-//       return [{},{}];
-//     };
-//   };
-// };
+export const battle = (state) => {
+  return (enemy) => {
+    return (prop) => {
+      if (state[prop]>enemy[prop]) {
+        return state;
+      }else if (state[prop]<enemy[prop]) {
+        return enemy;
+      }
+      return `There is a tie`;
+    };
+  };
+};
+
 
